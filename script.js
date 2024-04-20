aab0e2ec398ded7080ee67feb71f375b4e3a8c7e
Madhu <142790662+nmadhukarthik@users.noreply.github.com>
2024-04-21 00:11:23 +0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let student = { name: abc }
+student.prototype.getKeys = function() {
+	let arr = []
+	for(let item in student)
+	{ arr.push(item)}
+
+	return arr
+}
